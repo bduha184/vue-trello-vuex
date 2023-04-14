927c0a784018e61dfc61223f84d0002ac12e46b8
bduha184 <bduha_184@yahoo.co.jp>
2023-04-14 23:50:08 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+
+  <BaseBoard/>
+  </div>
+</template>
+
+<script>
+import BaseBoard from './components/BaseBoard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BaseBoard,
+  }
+}
+</script>
