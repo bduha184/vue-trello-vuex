fee2f1e1608a6d06f118affb827f83cb7dc1822a
bduha184 <bduha_184@yahoo.co.jp>
2023-04-15 22:40:39 +0900

    カードにdraggable機能追加

--- src/components/BaseBoard.vue
+++ src/components/BaseBoard.vue
@@ -3,7 +3,7 @@
     <header>my Trello</header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable class="list-index" :list="lists" @end="movingList">
         <TaskList
           v-for="(item, index) in lists"
           :key="item.id"
@@ -13,12 +13,13 @@
           @change="movingCard"
         />
         <ListAdd />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 import ListAdd from "./ListAdd.vue";
 import TaskList from "./TaskList.vue";
 import { mapState } from "vuex";
@@ -26,17 +27,21 @@ export default {
   components: {
     ListAdd,
     TaskList,
+    draggable,
   },
-  methods:{
-    movingCard(){
-      this.$store.dispatch('updateList',{lists:this.lists})
+  methods: {
+    movingCard() {
+      this.$store.dispatch("updateList", { lists: this.lists });
+    },
+    movingList(){
+      this.$store.dispatch('upgradeList',{lists:this.lists})
     }
   },
   computed: {
     ...mapState(["lists"]),
-    totalCardCount(){
-      return this.$store.getters.totalCardCount
-    }
+    totalCardCount() {
+      return this.$store.getters.totalCardCount;
+    },
   },
 };
 </script>
